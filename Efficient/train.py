2458d4304e820b3307e821248e49283efb143b29
DeepFocuser <58889565+DeepFocuser@users.noreply.github.com>
2020-06-15 01:04:54 +0900

    Update train.py

--- Efficient/train.py
+++ Efficient/train.py
@@ -259,8 +259,12 @@ def run(mean=[0.485, 0.456, 0.406],
 
     # optimizer weight 불러오기
     if os.path.exists(optimizer_path):
-        logging.info(f"loading {os.path.basename(optimizer_path)}\n")
-        trainer.load_states(optimizer_path)
+        try:
+            trainer.load_states(optimizer_path)
+        except Exception as E:
+            logging.info(E)
+        else:
+            logging.info(f"loading {os.path.basename(optimizer_path)}\n")
 
     '''
     localization loss -> Smooth L1 loss 
